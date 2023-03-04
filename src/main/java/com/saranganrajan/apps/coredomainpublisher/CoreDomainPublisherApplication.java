56c7ad723c28550768850772a5744d185df76b4f
saranganrajan <123263185+saranganrajan@users.noreply.github.com>
2023-03-04 13:28:18 +0800

    Add files via upload

---
+++ src/main/java/com/saranganrajan/apps/coredomainpublisher/CoreDomainPublisherApplication.java
@@ -0,0 +1,13 @@
+package com.saranganrajan.apps.coredomainpublisher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreDomainPublisherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreDomainPublisherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saranganrajan/apps/coredomainpublisher/CoreDomainPublisherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saranganrajan.apps.coredomainpublisher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreDomainPublisherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
