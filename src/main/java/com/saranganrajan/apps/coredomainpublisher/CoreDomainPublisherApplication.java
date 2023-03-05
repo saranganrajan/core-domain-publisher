5ac3a3702930b3b934cb77537a02304bff672fad
Sarangapani Varadharajan <sarangapani.singapore@gmail.com>
2023-03-06 03:21:19 +0800

    Implemented Azure Service Bus

--- src/main/java/com/saranganrajan/apps/coredomainpublisher/CoreDomainPublisherApplication.java
+++ src/main/java/com/saranganrajan/apps/coredomainpublisher/CoreDomainPublisherApplication.java
@@ -1,13 +1,53 @@
 package com.saranganrajan.apps.coredomainpublisher;
 
+import com.azure.spring.messaging.checkpoint.Checkpointer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Sinks;
+import static com.azure.spring.messaging.AzureHeaders.CHECKPOINTER;
+
+import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 @SpringBootApplication
-public class CoreDomainPublisherApplication {
+@Slf4j
+public class CoreDomainPublisherApplication implements CommandLineRunner {
+
+
+	private static final Sinks.Many<Message<String>> many = Sinks.many().unicast().onBackpressureBuffer();
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoreDomainPublisherApplication.class, args);
 	}
 
+	@Bean
+	public Supplier<Flux<Message<String>>> supply() {
+		return ()->many.asFlux()
+				.doOnNext(m->log.info("Manually sending message {}", m))
+				.doOnError(t->log.error("Error encountered", t));
+	}
+
+	@Bean
+	public Consumer<Message<String>> consume() {
+		return message->{
+			Checkpointer checkpointer = (Checkpointer) message.getHeaders().get(CHECKPOINTER);
+			log.info("New message received: '{}'", message.getPayload());
+			checkpointer.success()
+					.doOnSuccess(s->log.info("Message '{}' successfully checkpointed", message.getPayload()))
+					.doOnError(e->log.error("Error found", e))
+					.block();
+		};
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Going to add message {} to Sinks.Many.", "Hello Word");
+		many.emitNext(MessageBuilder.withPayload("Hello Word").build(), Sinks.EmitFailureHandler.FAIL_FAST);
+	}
 }
---
+++ src/main/java/com/saranganrajan/apps/coredomainpublisher/publisher/PolicyDomainPublisher.java
@@ -0,0 +1,4 @@
+package com.saranganrajan.apps.coredomainpublisher.publisher;
+
+public class PolicyDomainPublisher {
+}
